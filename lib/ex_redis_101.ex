f21de86f3eaf4e14df26c5e94569cf131ad1a535
hattori <hattori045>
2018-07-10 14:45:47 +0900

    課題制作 ex_redis_101.ex redisの読み書きを追加

--- lib/ex_redis_101.ex
+++ lib/ex_redis_101.ex
@@ -1,18 +1,55 @@
 defmodule ExRedis101 do
-  @moduledoc """
-  Documentation for ExRedis101.
-  """
 
-  @doc """
-  Hello world.
+	alias Redix
+	@moduledoc """
+	Documentation for ExRedis101.
+	"""
 
-  ## Examples
+	@doc """
+	Hello world.
 
-      iex> ExRedis101.hello
-      :world
+	## Examples
 
-  """
-  def hello do
-    :world
-  end
+		iex> ExRedis101.hello
+		:world
+
+	"""
+  	def hello do
+    	:world
+  	end
+
+  	def set(conn, key, value) do
+    	Redix.command(conn,["SET", key, value])
+  	end
+
+  	def get(conn, key) do
+    	Redix.command!(conn, ["GET", key])
+  	end
+
+
+  	defmodule User do
+    	def get_username_by_user_id(conn, id) do
+    	  to_string (Redix.command!(conn, ["HVALS", "user:#{id}"]))
+	    end
+
+		def next_user_id (conn) do
+    		Redix.command!(conn, ["INCR", 0])
+   		end
+  	end
+
+
+	defmodule AddWorker do
+		def preform(key, first, second) do
+			{:ok, con} = Redix.start_link()
+			{:ok, con: conn}
+			Redix.command(conn, ["SET", key, first + second])
+		end
+	end
+	
+
+	defmodule Ranking do
+		def get_ranking(conn) do
+			Redix.command!(conn, ["ZREVRANGE", "ranking", 0, -1])
+    	end
+  	end
 end
